a3b53bc6a6963fed86ace0740b84ab68089a3180
happine2s <happine2s@sookmyung.ac.kr>
2023-07-13 20:33:11 +0900

    [feat] 일반 사용자 회원가입
    https://eveningdev.tistory.com/20

--- starpool/userapp/views.py
+++ starpool/userapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib import auth
+from django.contrib.auth import authenticate
+from .models import User
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def signup(request):
+    if request.method=='POST':
+        if request.POST['password1']==request.POST['password2']:
+            user=User.objects.create_user(
+                email=request.POST['email'],
+                name=request.POST['name'],
+                password=request.POST['password1']
+            )
+            auth.login(request, user)
+            return redirect('/')
+        
+    return render(request, 'signup.html')
